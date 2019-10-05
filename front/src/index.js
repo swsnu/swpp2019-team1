c298326adfc8bc3e6e2bce8237808a76b20ac1fb
HaeTheong <platina47@naver.com>
2019-10-06 03:00:29 +0900

    Add eslint, prettier

--- front/src/index.js
+++ front/src/index.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
 
 const history = createBrowserHistory();
 const rootReducer = combineReducers({
-  // TODO add reducer
-  router: connectRouter(history)
-})
+  // TODO: add reducer
+  router: connectRouter(history),
+});
 
-const logger = store => {
-  return next => {
-    return action => {
-      console.log('[Middleware] Dispatching', action);
-      const result = next(action);
-      console.log('[Middleware] Next State', store.getState());
-      return result;
-    };
-  };
+const logger = store => next => action => {
+  console.log('[Middleware] Dispatching', action);
+  const result = next(action);
+  console.log('[Middleware] Next State', store.getState());
+  return result;
 };
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk, routerMiddleware(history)));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, thunk, routerMiddleware(history)),
+);
 
-ReactDOM.render(<Provider store={store}><App history={history}/></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
