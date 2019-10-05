b746eb9b5df902a93baf39e700028f16f683f7c4
DaeeunKim <eodmsabc@gmail.com>
2019-10-05 23:30:35 +0900

    Bug fixed

---
+++ front/src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  render() {
+    return (
+      <div className='Login'>
+        Login
+      </div>
+    )
+  }
+}
+
+export default Login;
