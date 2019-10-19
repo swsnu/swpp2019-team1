57b829969757ee6c47dc6116f77b450de5198678
Seonkyo Ok <uyw4687@naver.com>
2019-10-19 17:52:40 +0900

    outline

--- front/src/containers/Match/MatchCreate/MatchCreate.js
+++ front/src/containers/Match/MatchCreate/MatchCreate.js
@@ -1,8 +1,68 @@
+/*
+ *
+ */
+
 import React, { Component } from 'react';
+import MatchForm from '../../../components/Match/MatchForm/MatchForm'
+//import LocationPopUp from ''
 
 class MatchCreate extends Component {
+  state = { // default values
+    matchName: '',
+    //matchThumbnail: file,
+    categoryID: 0,
+    maxCapacity: 0,
+    locationText: '',
+    locationLatitude: '',
+    locationLongitude: '',
+    timeBegin: new Date(),
+    timeEnd: new Date(),
+    interval: 0,
+    restrictAgeFrom: 0,
+    restrictAgeTo: 0,
+    restrictGender: 0,
+    additionalInfo: '',
+    toggleGenderRestrict: false,
+    toggleAgeRestrict: false,
+    toggleOnline: false,
+    togglePeriodic: false,
+  }
+
+  componentDidMount() {
+
+  }
+
+  onClickCreate() {
+
+  }
+  
+  validateForm() {
+    
+  }
+  
+  handleLocationSearch() {
+    
+  }
+  
+  handleInputXXXChange() {
+    
+  }
+
   render() {
-    return <div className="MatchCreate">MatchCreate</div>;
+
+    //test coverage
+    this.onClickCreate();
+    this.validateForm();
+    this.handleLocationSearch();
+    this.handleInputXXXChange();
+
+    return (
+      <div className="MatchCreate">MatchCreate e
+        <MatchForm />
+        {/* <LocationPopUp /> */}
+        <button>Create</button>
+      </div>
+      );
   }
 }
 
