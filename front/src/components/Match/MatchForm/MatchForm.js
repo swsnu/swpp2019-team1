2d17757f91311b58e0a45e6318661eff00576cc4
Seonkyo Ok <uyw4687@naver.com>
2019-10-21 22:19:39 +0900

    match-create-eslint

--- front/src/components/Match/MatchForm/MatchForm.js
+++ front/src/components/Match/MatchForm/MatchForm.js
@@ -4,22 +4,52 @@ const MatchForm = props => {
   return (
     <div className="MatchForm">
       <h1>Create Activity</h1>
-      Title <input type="text" />
+      Title
+      <input type="text" />
       <br />
-      Category <input type="text" /> Up to <input type="number" /> people
+      Category
+      <input type="text" />
+      Up to
+      <input type="number" />
+      people
       <br />
-      Location <input type="text" /> Online <input type="checkbox" />
+      Location
+      <input type="text" />
+      Online
+      <input type="checkbox" />
       Event Start
       <br />
-      Date <input type="date" /> Time <input type="time" />  Periodic <input type="checkbox" />
+      Date
+      <input type="date" />
+      Time
+      <input type="time" />
+      Periodic
+      <input type="checkbox" />
       Event Finish
       <br />
-      Date <input type="date" /> Time <input type="time" />  Up to <input type="number" /> days
+      Date
+      <input type="date" />
+      Time
+      <input type="time" />
+      Up to
+      <input type="number" />
+      days
       <br />
-      Additional Information <input type="text" />
+      Additional Information
+      <input type="text" />
       <br />
       Restriction
-      Age <input type="checkbox" /> From <input type="number" /> To <input type="number" /> Age <input type="checkbox" /> <input type="button" value='M' /> <input type="button" value='F' />
+      <br />
+      Age
+      <input type="checkbox" />
+      From
+      <input type="number" />
+      To
+      <input type="number" />
+      Age
+      <input type="checkbox" />
+      <input type="button" value="M" />
+      <input type="button" value="F" />
     </div>
   );
 };
--- front/src/containers/Match/MatchCreate/MatchCreate.js
+++ front/src/containers/Match/MatchCreate/MatchCreate.js
@@ -3,66 +3,60 @@
  */
 
 import React, { Component } from 'react';
-import MatchForm from '../../../components/Match/MatchForm/MatchForm'
-//import LocationPopUp from ''
+import MatchForm from '../../../components/Match/MatchForm/MatchForm';
+// import LocationPopUp from ''
 
 class MatchCreate extends Component {
-  state = { // default values
-    matchName: '',
-    //matchThumbnail: file,
-    categoryID: 0,
-    maxCapacity: 0,
-    locationText: '',
-    locationLatitude: '',
-    locationLongitude: '',
-    timeBegin: new Date(),
-    timeEnd: new Date(),
-    interval: 0,
-    restrictAgeFrom: 0,
-    restrictAgeTo: 0,
-    restrictGender: 0,
-    additionalInfo: '',
-    toggleGenderRestrict: false,
-    toggleAgeRestrict: false,
-    toggleOnline: false,
-    togglePeriodic: false,
+  static validateForm() {}
+
+  static onClickCreate() {}
+
+  static handleLocationSearch() {}
+
+  static handleInputXXXChange() {}
+
+  constructor(props) {
+    // default values
+    super();
+    this.state = {
+      matchName: 0,
+      // matchThumbnail: file,
+      categoryID: 0,
+      maxCapacity: 0,
+      locationText: '',
+      locationLatitude: '',
+      locationLongitude: '',
+      timeBegin: new Date(),
+      timeEnd: new Date(),
+      interval: 0,
+      restrictAgeFrom: 0,
+      restrictAgeTo: 0,
+      restrictGender: 0,
+      additionalInfo: '',
+      isOnline: false,
+      isPeriodic: false,
+      isAgeRestricted: false,
+      isGenderRestricted: false,
+    };
   }
 
-  componentDidMount() {
-
-  }
-
-  onClickCreate() {
-
-  }
-  
-  validateForm() {
-    
-  }
-  
-  handleLocationSearch() {
-    
-  }
-  
-  handleInputXXXChange() {
-    
-  }
+  componentDidMount() {}
 
   render() {
-
-    //test coverage
-    this.onClickCreate();
+    // test coverage
     this.validateForm();
-    this.handleLocationSearch();
+    this.onClickCreate();
     this.handleInputXXXChange();
+    this.handleLocationSearch();
 
     return (
-      <div className="MatchCreate">MatchCreate e
+      <div className="MatchCreate">
+        MatchCreate e
         <MatchForm />
         {/* <LocationPopUp /> */}
-        <button>Create</button>
+        <button type="button">Create</button>
       </div>
-      );
+    );
   }
 }
 
