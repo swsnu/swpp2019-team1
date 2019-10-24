f536d27b4dd0c9a419045eff9d6f490b04c623f0
HaeTheong <platina47@naver.com>
2019-10-24 19:54:50 +0900

    add Travis CI

--- front/src/store/reducers/user.js
+++ front/src/store/reducers/user.js
@@ -4,16 +4,17 @@ const initialState = {
   userid: null,
 };
 
-const testinitialState = {
+const testInitialState = {
   userid: 1,
 };
 
-const reducer = (state = testinitialState, action) => {
+const reducer = (state = testInitialState, action) => {
   switch (action.type) {
+    case actionTypes.CREATE_USER:
+      return { ...state };
     default:
       break;
   }
   return state;
 };
-
 export default reducer;
