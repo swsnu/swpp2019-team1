546ef8f0b19c2ce76c0af0a57bc3f4f17da8cfd3
HaeTheong <platina47@naver.com>
2019-12-14 19:20:32 +0900

    Fix css

--- front/src/components/Map/SearchBox.test.js
+++ front/src/components/Map/SearchBox.test.js
@@ -1,80 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { Route, Switch } from 'react-router-dom';
-import moment from 'moment';
-
-import getMockStore from '../../test-utils/getMockStore';
 import SearchBox from './SearchBox';
-import { history } from '../../store/store';
-
-const stubUser = {};
-const stubMatch = {
-  selected: {
-    id: 1,
-    hostUser: {
-      id: 1,
-      username: 'TEST_HOST_USER',
-    },
-    title: 'TEST_TITLE',
-    hostName: 'TEST_HOSTNAME',
-    additionalInfo: 'TEST_ADITIONAL_INFO',
-    // matchThumbnail
-    category: [0, 0],
-    capacity: 2,
-    isOnline: false,
-    locationText: '',
-    locationLatitude: 0,
-    locationLongitude: 0,
-    timeBegin: moment('2019-11-07T00:35:38.334Z'),
-    timeEnd: moment(),
-    isPeriodic: false,
-    period: 0,
-    isAgeRestricted: false,
-    restrictAgeFrom: 0,
-    restrictAgeTo: 0,
-    isGenderRestricted: false,
-    restrictToMale: false,
-    restrictToFemale: false,
-    numParticipants: 1,
-  },
-  location: '301-408',
-};
-const stubMatchBlankLocation = {
-  selected: {
-    id: 1,
-    hostUser: {
-      id: 1,
-      username: 'TEST_HOST_USER',
-    },
-    title: 'TEST_TITLE',
-    hostName: 'TEST_HOSTNAME',
-    additionalInfo: 'TEST_ADITIONAL_INFO',
-    // matchThumbnail
-    category: [0, 0],
-    capacity: 2,
-    isOnline: false,
-    locationText: '',
-    locationLatitude: 0,
-    locationLongitude: 0,
-    timeBegin: moment('2019-11-07T00:35:38.334Z'),
-    timeEnd: moment(),
-    isPeriodic: false,
-    period: 0,
-    isAgeRestricted: false,
-    restrictAgeFrom: 0,
-    restrictAgeTo: 0,
-    isGenderRestricted: false,
-    restrictToMale: false,
-    restrictToFemale: false,
-    numParticipants: 1,
-  },
-  location: '',
-};
-const mockStore = getMockStore(stubUser, stubMatch);
-const mockStoreBlankLocation = getMockStore(stubUser, stubMatchBlankLocation);
 
 describe('<SearchBox />', () => {
   let searchBox;
@@ -97,61 +24,28 @@ describe('<SearchBox />', () => {
   const stubOnPlacesChanged = jest.fn(() => {});
   beforeEach(() => {
     searchBox = (
-      <Provider store={mockStore}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={() => (
-                <SearchBox
-                  map={stubMapInstance}
-                  mapsApi={stubMapsApi}
-                  onPlacesChanged={stubOnPlacesChanged}
-                />
-              )}
-            />
-          </Switch>
-        </ConnectedRouter>
-      </Provider>
+      <SearchBox
+        map={stubMapInstance}
+        mapsApi={stubMapsApi}
+        onPlacesChanged={stubOnPlacesChanged}
+        placeholder="301-408"
+      />
     );
     searchBoxWithoutOnPlacesChanged = (
-      <Provider store={mockStore}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={() => (
-                <SearchBox
-                  map={stubMapInstance}
-                  mapsApi={stubMapsApi}
-                  onPlacesChanged={null}
-                />
-              )}
-            />
-          </Switch>
-        </ConnectedRouter>
-      </Provider>
+      <SearchBox
+        map={stubMapInstance}
+        mapsApi={stubMapsApi}
+        onPlacesChanged={null}
+        placeholder="301-408"
+      />
     );
     searchBoxBlankLocation = (
-      <Provider store={mockStoreBlankLocation}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={() => (
-                <SearchBox
-                  map={stubMapInstance}
-                  mapsApi={stubMapsApi}
-                  onPlacesChanged={null}
-                />
-              )}
-            />
-          </Switch>
-        </ConnectedRouter>
-      </Provider>
+      <SearchBox
+        map={stubMapInstance}
+        mapsApi={stubMapsApi}
+        onPlacesChanged={stubOnPlacesChanged}
+        placeholder=""
+      />
     );
   });
   afterEach(() => {
@@ -168,7 +62,7 @@ describe('<SearchBox />', () => {
   it('should clear instance listeners when component will unmount', () => {
     const component = mount(searchBox);
     component
-      .find(SearchBox.WrappedComponent)
+      .find(SearchBox)
       .instance()
       .componentWillUnmount();
     expect(stubClearInstanceListeners).toHaveBeenCalledTimes(1);
@@ -177,7 +71,7 @@ describe('<SearchBox />', () => {
   it('should call getPlaces when onPlacesChanged is not null', () => {
     const component = mount(searchBox);
     component
-      .find(SearchBox.WrappedComponent)
+      .find(SearchBox)
       .instance()
       .onPlacesChanged();
     expect(stubGetPlaces).toHaveBeenCalledTimes(1);
@@ -186,7 +80,7 @@ describe('<SearchBox />', () => {
   it('should not call getPlaces when onPlacesChanged is null', () => {
     const component = mount(searchBoxWithoutOnPlacesChanged);
     component
-      .find(SearchBox.WrappedComponent)
+      .find(SearchBox)
       .instance()
       .onPlacesChanged();
     expect(stubGetPlaces).toHaveBeenCalledTimes(0);
